4c1894f81cddaca91de328d5dc2c6ce12aa018c1
Jannis Gull <jannis.gull@ost.ch>
2023-10-23 15:46:26 +0200

    prototype main page

--- CalcApplication/main.cpp
+++ CalcApplication/main.cpp
@@ -1,11 +1,24 @@
 #include "MainWindow.h"
 
 #include <QApplication>
+#include <QtWidgets>
 
 int main(int argc, char* argv[])
 {
-  QApplication a(argc, argv);
-  MainWindow w;
-  w.show();
-  return a.exec();
+  QApplication app(argc, argv);
+  QDialog mainWindow;
+  QVBoxLayout* mainLayout = new QVBoxLayout;
+  mainWindow.setLayout(mainLayout);
+  QLabel* label = new QLabel("Electronics Calculator");
+  mainLayout->addWidget(label);
+  QHBoxLayout* bottomLayout = new QHBoxLayout;
+  mainLayout->addLayout(bottomLayout);
+  QPushButton* calc1Button = new QPushButton("Calc_1");
+  bottomLayout->addWidget(calc1Button);
+  QPushButton* calc2Button = new QPushButton("Calc_2");
+  bottomLayout->addWidget(calc2Button);
+  QPushButton* calc3Button = new QPushButton("Calc_3");
+  bottomLayout->addWidget(calc3Button);
+  mainWindow.show();
+  return app.exec();
 }
