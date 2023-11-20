30e070ed4358636238d604bd5a26b209554fa23b
Jannis Gull <jannis.gull@ost.ch>
2023-11-20 16:45:59 +0100

    Design RL komplettiert und ein ausgabe eingefügt

--- CalcApplication/BerechnungRL.cpp
+++ CalcApplication/BerechnungRL.cpp
@@ -1,10 +1,13 @@
 #include "BerechnungRL.h"
+#include "QDebug"
 #include "ui_BerechnungRL.h"
 
 BerechnungRL::BerechnungRL(QWidget* parent)
     : QWidget(parent), ui(new Ui::BerechnungRL)
 {
   ui->setupUi(this);
+  connect(ui->calculate, &QPushButton::pressed, this,
+          &BerechnungRL::onPbPressed);
 }
 
 BerechnungRL::~BerechnungRL()
--- CalcApplication/BerechnungRL.h
+++ CalcApplication/BerechnungRL.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 #include <QWidget>
+#include "QDebug"
+#include "ui_BerechnungRL.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui
@@ -18,6 +20,16 @@ class BerechnungRL : public QWidget
  public:
   BerechnungRL(QWidget* parent = nullptr);
   ~BerechnungRL();
+
+ private slots:
+  void onPbPressed()
+  {
+    QString spannung = ui->spannungIn->text();
+    QString frequenz = ui->frequenzIn->text();
+    QString widerstand = ui->widerstandIn->text();
+    QString induktivitaet = ui->induktivitaetIn->text();
+    ui->stromOut->setText(spannung);
+  }
 
  private:
   Ui::BerechnungRL* ui;
