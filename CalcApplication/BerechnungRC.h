a72f35ea49579b06b000a6f848f32b0e20864750
Yannick Flepp <yannick.flepp@ost.ch>
2023-11-28 19:03:50 +0100

    Berechnung bei allen Schaltungen ergänzt

--- CalcApplication/BerechnungRC.h
+++ CalcApplication/BerechnungRC.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QWidget>
+#include <QtMath>
 #include "Error.h"
 #include "ui_BerechnungRC.h"
 
@@ -26,27 +27,27 @@ class BerechnungRC : public QWidget
   void onPbPressed()
   {
     bool ok = false;
-    double spannung = (ui->spannungIn->text()).toDouble(&ok);
-    double frequenz = (ui->frequenzIn->text()).toDouble(&ok);
-    double widerstand = (ui->widerstandIn->text()).toDouble(&ok);
-    double induktivitaet = (ui->kapazitaetIn->text()).toDouble(&ok);
+    double u = (ui->spannungIn->text()).toDouble(&ok);
+    double f = (ui->frequenzIn->text()).toDouble(&ok);
+    double r = (ui->widerstandIn->text()).toDouble(&ok);
+    double c = (ui->kapazitaetIn->text()).toDouble(&ok);
     if (ok == false)
     {
       error.show();
     }
-    double strom = 0;
-    double impedanz = 0;
-    double phasenwinkel = 0;
-    double leistung = 0;
-    double blindleistung = 0;
-    double scheinleistung = 0;
+    double z = qSqrt(qPow(r, 2) + qPow(1 / (2 * M_PI * f * c), 2));
+    double i = u / z;
+    double p = r * qPow(i, 2);
+    double q = (1 / (2 * M_PI * f * c)) * qPow(i, 2);
+    double s = u * i;
+    double phi = (qAcos((p / s)) / (2 * M_PI)) * 360;
 
-    ui->stromOut->setText(QString::number(strom, 'f', 2));
-    ui->impedanzOut->setText(QString::number(impedanz, 'f', 2));
-    ui->phasenwinkelOut->setText(QString::number(phasenwinkel, 'f', 2));
-    ui->leistungOut->setText(QString::number(leistung, 'f', 2));
-    ui->blindleistungOut->setText(QString::number(blindleistung, 'f', 2));
-    ui->scheinleistungOut->setText(QString::number(scheinleistung, 'f', 2));
+    ui->stromOut->setText(QString::number(i, 'f', 2));
+    ui->impedanzOut->setText(QString::number(z, 'f', 2));
+    ui->phasenwinkelOut->setText(QString::number(phi, 'f', 2));
+    ui->leistungOut->setText(QString::number(p, 'f', 2));
+    ui->blindleistungOut->setText(QString::number(q, 'f', 2));
+    ui->scheinleistungOut->setText(QString::number(s, 'f', 2));
   }
 
  private:
--- CalcApplication/BerechnungRLC.h
+++ CalcApplication/BerechnungRLC.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QWidget>
+#include <QtMath>
 #include "Error.h"
 #include "ui_BerechnungRLC.h"
 
@@ -26,21 +27,33 @@ class BerechnungRLC : public QWidget
   void onPbPressed()
   {
     bool ok = false;
-    double spannung = (ui->spannungIn->text()).toDouble(&ok);
-    double frequenz = (ui->frequenzIn->text()).toDouble(&ok);
-    double widerstand = (ui->widerstandIn->text()).toDouble(&ok);
-    double induktivitaet = (ui->induktivitaetIn->text()).toDouble(&ok);
-    double kapazitaet = (ui->kapazitaetIn->text()).toDouble(&ok);
+    double u = (ui->spannungIn->text()).toDouble(&ok);
+    double f = (ui->frequenzIn->text()).toDouble(&ok);
+    double r = (ui->widerstandIn->text()).toDouble(&ok);
+    double l = (ui->induktivitaetIn->text()).toDouble(&ok);
+    double c = (ui->kapazitaetIn->text()).toDouble(&ok);
     if (ok == false)
     {
       error.show();
     }
-    ui->stromOut->setText(QString::number(spannung / 2, 'f', 5));
-    ui->impedanzOut->setText(QString::number(frequenz / 2, 'f', 5));
-    ui->phasenwinkelOut->setText(QString::number(induktivitaet / 2, 'f', 5));
-    ui->leistungOut->setText(QString::number(widerstand / 2, 'f', 5));
-    ui->blindleistungOut->setText(QString::number(spannung / 2, 'f', 5));
-    ui->scheinleistungOut->setText(QString::number(spannung / 2, 'f', 5));
+    double x = (2 * M_PI * f * l) - (1 / (2 * M_PI * f * c));
+    if (x <= 0)
+    {
+      x = x * -1;
+    };
+    double z = qSqrt(qPow(r, 2) + qPow(x, 2));
+    double i = u / z;
+    double p = r * qPow(i, 2);
+    double q = x * qPow(i, 2);
+    double s = u * i;
+    double phi = (qAcos((p / s)) / (2 * M_PI)) * 360;
+
+    ui->stromOut->setText(QString::number(i, 'f', 2));
+    ui->impedanzOut->setText(QString::number(z, 'f', 2));
+    ui->phasenwinkelOut->setText(QString::number(phi, 'f', 2));
+    ui->leistungOut->setText(QString::number(p, 'f', 2));
+    ui->blindleistungOut->setText(QString::number(q, 'f', 2));
+    ui->scheinleistungOut->setText(QString::number(s, 'f', 2));
   }
 
  private:
